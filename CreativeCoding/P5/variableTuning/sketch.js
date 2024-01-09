64231aa2cf14e47bfb0570f7067ebc3fac7921c4
Reed Gilbert <jaxmandoge@gmail.com>
2024-01-09 11:19:36 -0500

    variablesTuning

---
+++ CreativeCoding/P5/variableTuning/sketch.js
@@ -0,0 +1,24 @@
+function setup() { //runs one time
+createCanvas(windowWidth,windowWidth);
+  fill("red");
+  strokeWeight(1);
+}
+
+function draw() { //runs in a loop
+  
+  var num = 20; //num squares
+  var sideLen = windowWidth/num; //side length of each square
+  
+  
+  for(var y=0; y<windowWidth; y+=sideLen)
+    {
+      for(var x=0; x<windowWidth; x+=sideLen)
+        {
+          quad (x,y,
+               x+sideLen, y, 
+               x+sideLen , y+sideLen,
+               x, y+sideLen);
+        }
+    }
+
+}
