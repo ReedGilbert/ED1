c2d7f5f7446a306aabce730be76e4e29fbbf01d6
Reed Gilbert <jaxmandoge@gmail.com>
2024-01-29 10:10:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ gif = loadImage ('assets/GilbertCatGif.gif');
 
 function setup() {
   //runs one time
-  createCanvas(windowWidth*2, windowHeight*2);
+  createCanvas(windowWidth, windowHeight*2);
   fill("lightyellow");
   strokeWeight(1);
 }
@@ -21,7 +21,7 @@ function draw() {
   var num = 10; //num squares
   var sideLen = windowWidth / num; //side length of each square
 
-  for (var y = 0; y < windowHeight * 2; y += sideLen) {
+  for (var y = 0; y < windowHeight; y += sideLen) {
     for (var x = 0; x < windowWidth * 2; x += sideLen) {
       image(gif, x, y, sideLen, sideLen);
     }
